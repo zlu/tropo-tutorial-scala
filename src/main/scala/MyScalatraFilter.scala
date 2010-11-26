f1b0c79d6ce334e5f17038023d1a6fb7c0ff0c6c
zhao lu <zlu@me.com>
2010-11-26 01:28:18 -0800

    initial check in

--- src/main/scala/MyScalatraFilter.scala
+++ src/main/scala/MyScalatraFilter.scala
@@ -1,18 +1,62 @@
 package com.example
 
+import scala.xml.{Text, Node}
 import org.scalatra._
 import java.net.URL
 import scalate.ScalateSupport
+import scala.io.Source.fromInputStream
+import java.net.URL
 
-class MyScalatraFilter extends ScalatraFilter with ScalateSupport {
+class MyScalatraFilter extends ScalatraFilter with ScalateSupport with UrlSupport{
+
+  object Template {
+
+    def style() = 
+      """
+      pre { border: 1px solid black; padding: 10px; } 
+      body { font-family: Helvetica, sans-serif; } 
+      h1 { color: #8b2323 }
+      """
+
+    def page(title:String, content:Seq[Node]) = {
+      <html>
+        <head>
+          <title>{ title }</title>
+          <style>{ Template.style }</style>
+        </head>
+        <body>
+          <h1>{ title }</h1>
+          { content }
+          <hr/>
+          <a href={url("/")}>Sending SMS using Tropo With Scalatra</a>
+        </body>
+      </html>
+    }
+  }
   
-  get("/") {
-    <html>
-      <body>
-        <h1>Hello, world!</h1>
-        Say <a href="hello-scalate">hello to Scalate</a>.
-      </body>
-    </html>
+  get("/") {        
+	Template.page("Sending SMS Using Tropo With Scalatra",
+	<form action={url("/send")} method='POST'>
+	  Phone Number: <input name='to' type='text'/><br />
+	  Message: <input name='msg' type='text'/><br />
+	  <input type='submit'/>
+         </form>
+         <pre>Route: /</pre>
+	 )
+  }
+
+  post("/send") {
+    var tropoURL = "http://api.tropo.com/1.0/sessions?action=create"
+    val tropoToken = "1aba4b151514ae4caaf8340879b3e456893a5f5f7d13be43d5df9546c147090c4773a68016b0dae4da7d66bc"
+java.lang.System.out.println(request.getAttribute("to"))
+    tropoURL += "&to=" + request.getParameter("to").toString() + "&msg=" + request.getParameter("msg").toString() + "&token=" + tropoToken
+    val url = new URL(tropoURL)
+    fromInputStream(url.openStream).getLines.foreach(print)
+
+    Template.page("SMS Send",
+    <p>You send to: {params("to")} with the following message: {params("msg")}</p>
+    <pre>Route: /send</pre>
+    )
   }
 
   notFound {
@@ -30,4 +74,6 @@ class MyScalatraFilter extends ScalatraFilter with ScalateSupport {
         filterChain.doFilter(request, response)
     } 
   }
+
+  protected def contextPath = request.getContextPath
 }
